ee67f707a18692116e970ff7e53bf001ce4af329
Romain Vialard <romain.vialard@revevol.eu>
2017-11-03 11:26:45 +0000

    Code.gs.js edited online with Bitbucket

--- src/Code.gs.js
+++ src/Code.gs.js
@@ -15,11 +15,10 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
-var FirebaseApp_ = {
-};
+var FirebaseApp_ = {};
 
 FirebaseApp_.base = function (base) {
-	this.base = base;
+  this.base = base;
 };
 
 var baseClass_ = FirebaseApp_.base.prototype;
@@ -32,12 +31,12 @@ var baseClass_ = FirebaseApp_.base.prototype;
  * @return {Database} the Database found at the given URL
  */
 function getDatabaseByUrl(url, optSecret) {
-	if (!optSecret) optSecret = '';
-	var base = new FirebaseApp_.base({
-		url: url,
-		secret: optSecret
-	});
-	return base;
+  if (!optSecret) optSecret = '';
+  var base = new FirebaseApp_.base({
+    url: url,
+    secret: optSecret
+  });
+  return base;
 };
 
 /**
@@ -49,20 +48,22 @@ function getDatabaseByUrl(url, optSecret) {
  * @param  {string} privateKey the private key of this service account
  * @return {object} the auth token granting access to firebase
  */
-baseClass_.createAuthToken = function(userEmail,optAuthData,serviceAccountEmail,privateKey){
-	if(arguments.length > 2){ //more then two means they want to use a service account
-		if(typeof arguments[1] === "string"){ // no optional data
-			this.base.serviceAccountEmail =  arguments[1];
-			this.base.privateKey = arguments[2];
-			optAuthData = {};
-		}else if(typeof arguments[1] === "object") { // optional data is present
-			this.base.serviceAccountEmail =  serviceAccountEmail;
-			this.base.privateKey = privateKey;
-		}
-		return this.createAuthTokenFromServiceAccount_(userEmail,optAuthData);
-	}else{
-		return this.createLegacyAuthToken_(userEmail,optAuthData);
-	}
+baseClass_.createAuthToken = function (userEmail, optAuthData, serviceAccountEmail, privateKey) {
+  if (arguments.length > 2) { //more then two means they want to use a service account
+    if (typeof arguments[1] === "string") { // no optional data
+      this.base.serviceAccountEmail = arguments[1];
+      this.base.privateKey = arguments[2];
+      optAuthData = {};
+    }
+    else if (typeof arguments[1] === "object") { // optional data is present
+      this.base.serviceAccountEmail = serviceAccountEmail;
+      this.base.privateKey = privateKey;
+    }
+    return this.createAuthTokenFromServiceAccount_(userEmail, optAuthData);
+  }
+  else {
+    return this.createLegacyAuthToken_(userEmail, optAuthData);
+  }
 }
 
 
@@ -73,47 +74,47 @@ baseClass_.createAuthToken = function(userEmail,optAuthData,serviceAccountEmail,
  * @param  {object} optAuthData keypairs of data to be associated to this user.
  * @return {object} the auth token granting access to firebase
  */
-baseClass_.createAuthTokenFromServiceAccount_ = function(userEmail, optAuthData) {
-	if(!("serviceAccountEmail" in this.base) || !("privateKey" in this.base)){
-		throw Error("You must provide both the serviceEmailAccount and the privateKey to generate a token")
-	};
-	// Specific YAMM
-	if (!optAuthData) {
-		var tmp = userEmail.split('@');
-		var username = tmp[0];
-		var domain = tmp[1];
-		optAuthData = {
-			domain: domain.replace(/\./g, '-'),
-			username: username.replace(/^0+/, '').replace(/\./g, '-'),
-			emailAddress: userEmail
-		}
-	}
+baseClass_.createAuthTokenFromServiceAccount_ = function (userEmail, optAuthData) {
+  if (!("serviceAccountEmail" in this.base) || !("privateKey" in this.base)) {
+    throw Error("You must provide both the serviceEmailAccount and the privateKey to generate a token")
+  };
+  // Specific YAMM
+  if (!optAuthData) {
+    var tmp = userEmail.split('@');
+    var username = tmp[0];
+    var domain = tmp[1];
+    optAuthData = {
+      domain: domain.replace(/\./g, '-'),
+      username: username.replace(/^0+/, '').replace(/\./g, '-'),
+      emailAddress: userEmail
+    }
+  }
 
-	var header = JSON.stringify({
-		"typ": "JWT",
-		"alg": "RS256"
-	});
-	header = Utilities.base64EncodeWebSafe(header);
-	var now = Math.floor((new Date).getTime() / 1E3);
-	var body = {
-		"iss": this.base.serviceAccountEmail,
-		"sub": this.base.serviceAccountEmail,
-		"aud":"https://identitytoolkit.googleapis.com/google.identity.identitytoolkit.v1.IdentityToolkit",
-		"iat": now,
-		"exp":now + 3600,
-		"uid": userEmail.replace(/[|&;$%@"<>()+,\.]/g, ""),
-		"claims":{}
-	};
+  var header = JSON.stringify({
+    "typ": "JWT",
+    "alg": "RS256"
+  });
+  header = Utilities.base64EncodeWebSafe(header);
+  var now = Math.floor((new Date).getTime() / 1E3);
+  var body = {
+    "iss": this.base.serviceAccountEmail,
+    "sub": this.base.serviceAccountEmail,
+    "aud": "https://identitytoolkit.googleapis.com/google.identity.identitytoolkit.v1.IdentityToolkit",
+    "iat": now,
+    "exp": now + 3600,
+    "uid": userEmail.replace(/[|&;$%@"<>()+,\.]/g, ""),
+    "claims": {}
+  };
 
-	if(optAuthData){
-		Object.keys(optAuthData).forEach(function (item) {
-			body.claims[item] = optAuthData[item];
-		});
-	}
-	body = JSON.stringify(body);  //Stringified after adding optional auth data
-	body = Utilities.base64Encode(body);
-	var signature = Utilities.computeRsaSha256Signature(header + "." + body, this.base.privateKey);
-	return header + "." + body + "." + Utilities.base64Encode(signature);
+  if (optAuthData) {
+    Object.keys(optAuthData).forEach(function (item) {
+      body.claims[item] = optAuthData[item];
+    });
+  }
+  body = JSON.stringify(body); //Stringified after adding optional auth data
+  body = Utilities.base64Encode(body);
+  var signature = Utilities.computeRsaSha256Signature(header + "." + body, this.base.privateKey);
+  return header + "." + body + "." + Utilities.base64Encode(signature);
 };
 
 /**
@@ -123,40 +124,40 @@ baseClass_.createAuthTokenFromServiceAccount_ = function(userEmail, optAuthData)
  * @param  {object} optAuthData keypairs of data to be associated to this user.
  * @return {object} the auth token granting access to firebase
  */
-baseClass_.createLegacyAuthToken_ = function(userEmail, optAuthData) {
-	// Specific YAMM
-	if (!optAuthData) {
-		var tmp = userEmail.split('@');
-		var username = tmp[0];
-		var domain = tmp[1];
-		optAuthData = {
-			domain: domain.replace(/\./g, '-'),
-			username: username.replace(/^0+/, '').replace(/\./g, '-'),
-			emailAddress: userEmail
-		}
-	}
-	var header = JSON.stringify({
-		"typ": "JWT",
-		"alg": "HS256"
-	});
-	header = Utilities.base64EncodeWebSafe(header);
-	var payload = {
-		"v": 0,
-		"d": {
-			"uid": userEmail.replace(/[|&;$%@"<>()+,\.]/g, "")
-		},
-		// iat : 'issued at' in second
-		"iat": Math.floor((new Date).getTime() / 1E3)
-	};
-	if(optAuthData){
-		Object.keys(optAuthData).forEach(function (item) {
-			payload.d[item] = optAuthData[item];
-		});
-	}
-	payload = JSON.stringify(payload);  //Stringified after adding optional auth data
-	payload = Utilities.base64EncodeWebSafe(payload);
-	var hmac = Utilities.computeHmacSha256Signature(header + "." + payload, this.base.secret);
-	return header + "." + payload + "." + Utilities.base64EncodeWebSafe(hmac);
+baseClass_.createLegacyAuthToken_ = function (userEmail, optAuthData) {
+  // Specific YAMM
+  if (!optAuthData) {
+    var tmp = userEmail.split('@');
+    var username = tmp[0];
+    var domain = tmp[1];
+    optAuthData = {
+      domain: domain.replace(/\./g, '-'),
+      username: username.replace(/^0+/, '').replace(/\./g, '-'),
+      emailAddress: userEmail
+    }
+  }
+  var header = JSON.stringify({
+    "typ": "JWT",
+    "alg": "HS256"
+  });
+  header = Utilities.base64EncodeWebSafe(header);
+  var payload = {
+    "v": 0,
+    "d": {
+      "uid": userEmail.replace(/[|&;$%@"<>()+,\.]/g, "")
+    },
+    // iat : 'issued at' in second
+    "iat": Math.floor((new Date).getTime() / 1E3)
+  };
+  if (optAuthData) {
+    Object.keys(optAuthData).forEach(function (item) {
+      payload.d[item] = optAuthData[item];
+    });
+  }
+  payload = JSON.stringify(payload); //Stringified after adding optional auth data
+  payload = Utilities.base64EncodeWebSafe(payload);
+  var hmac = Utilities.computeHmacSha256Signature(header + "." + payload, this.base.secret);
+  return header + "." + payload + "." + Utilities.base64EncodeWebSafe(hmac);
 };
 
 /**
@@ -167,7 +168,7 @@ baseClass_.createLegacyAuthToken_ = function(userEmail, optAuthData) {
  * @return {object} the data found at the given path
  */
 baseClass_.getData = function (path, optQueryParameters) {
-	return FirebaseApp_._buildRequest("get", this.base, path, null, optQueryParameters);
+  return FirebaseApp_._buildRequest("get", this.base, path, null, optQueryParameters);
 };
 
 /**
@@ -179,7 +180,7 @@ baseClass_.getData = function (path, optQueryParameters) {
  * @return {string} the child name of the new data that was added
  */
 baseClass_.pushData = function (path, data, optQueryParameters) {
-	return FirebaseApp_._buildRequest("post", this.base, path, data, optQueryParameters);
+  return FirebaseApp_._buildRequest("post", this.base, path, data, optQueryParameters);
 };
 
 /**
@@ -191,7 +192,7 @@ baseClass_.pushData = function (path, data, optQueryParameters) {
  * @return {object} the data written
  */
 baseClass_.setData = function (path, data, optQueryParameters) {
-	return FirebaseApp_._buildRequest("put", this.base, path, data, optQueryParameters);
+  return FirebaseApp_._buildRequest("put", this.base, path, data, optQueryParameters);
 };
 
 /**
@@ -203,7 +204,7 @@ baseClass_.setData = function (path, data, optQueryParameters) {
  * @return {object} the data written
  */
 baseClass_.updateData = function (path, data, optQueryParameters) {
-	return FirebaseApp_._buildRequest("patch", this.base, path, data, optQueryParameters);
+  return FirebaseApp_._buildRequest("patch", this.base, path, data, optQueryParameters);
 };
 
 /**
@@ -214,89 +215,89 @@ baseClass_.updateData = function (path, data, optQueryParameters) {
  * @return {null}
  */
 baseClass_.removeData = function (path, optQueryParameters) {
-	return FirebaseApp_._buildRequest("delete", this.base, path, null, optQueryParameters);
+  return FirebaseApp_._buildRequest("delete", this.base, path, null, optQueryParameters);
 };
 
 FirebaseApp_._buildRequest = function (method, base, path, data, optQueryParameters) {
-	if (optQueryParameters && typeof optQueryParameters != "object") {
-		throw new Error("optQueryParameters must be an object");
-	}
-	if (!path) path = '';
-	var params = {
-		method: method,
-		headers: {},
-		muteHttpExceptions: true
-	}
-	var url = base.url + path + ".json";
-	if (optQueryParameters) {
-		url += "?";
-		if (base.secret !== "") {
-			if ("auth" in optQueryParameters) optQueryParameters["auth"] = base.secret;
-			else url += "auth=" + base.secret + "&";
-		}
-		var parameters = [];
-		for (var key in optQueryParameters) {
-			if (key != "auth" && key != "shallow" && key != "print" && key != "limitToFirst" && key != "limitToLast") {
-				if (isNaN(optQueryParameters[key]) && typeof optQueryParameters[key] !== 'boolean') {
-					optQueryParameters[key] = encodeURIComponent('"' + optQueryParameters[key] + '"');
-				}
-			}
-			parameters.push(key + "=" + optQueryParameters[key]);
-		}
-		url += parameters.join("&");
-	}
-	else if (base.secret !== "") {
-		url += "?auth=" + base.secret;
-	}
-	if (data || data == 0) params.payload = JSON.stringify(data);
-	if (method === "patch") {
-		params.headers["X-HTTP-Method-Override"] = "PATCH";
-		params.method = "post";
-	}
-	// Exponential backoff is needed as server errors are more and more common on Firebase
-	for (var n = 0; n < 6; n++) {
-		var result = FirebaseApp_._sendRequest(url, params);
-		if (!result) return;
-		else if (!(result instanceof Error)) break;
-		else {
-			if (n == 5) {
-				throw result;
-			}
-			Utilities.sleep((Math.pow(2,n)*1000) + (Math.round(Math.random() * 1000)));
-		}
-	}
-	if (method == "post" && JSON.parse(result)['name']) return JSON.parse(result)['name'];
-	// Sometimes JSON.parse() fails with "Unexpected token: <"
-	try {
-		return JSON.parse(result);
-	}
-	catch(e) {
-		throw new Error(result);
-	}
+  if (optQueryParameters && typeof optQueryParameters != "object") {
+    throw new Error("optQueryParameters must be an object");
+  }
+  if (!path) path = '';
+  var params = {
+    method: method,
+    headers: {},
+    muteHttpExceptions: true
+  }
+  var url = base.url + path + ".json";
+  if (optQueryParameters) {
+    url += "?";
+    if (base.secret !== "") {
+      if ("auth" in optQueryParameters) optQueryParameters["auth"] = base.secret;
+      else url += "auth=" + base.secret + "&";
+    }
+    var parameters = [];
+    for (var key in optQueryParameters) {
+      if (key != "auth" && key != "shallow" && key != "print" && key != "limitToFirst" && key != "limitToLast") {
+        if (isNaN(optQueryParameters[key]) && typeof optQueryParameters[key] !== 'boolean') {
+          optQueryParameters[key] = encodeURIComponent('"' + optQueryParameters[key] + '"');
+        }
+      }
+      parameters.push(key + "=" + optQueryParameters[key]);
+    }
+    url += parameters.join("&");
+  }
+  else if (base.secret !== "") {
+    url += "?auth=" + base.secret;
+  }
+  if (data || data == 0) params.payload = JSON.stringify(data);
+  if (method === "patch") {
+    params.headers["X-HTTP-Method-Override"] = "PATCH";
+    params.method = "post";
+  }
+  // Exponential backoff is needed as server errors are more and more common on Firebase
+  for (var n = 0; n < 6; n++) {
+    var result = FirebaseApp_._sendRequest(url, params);
+    if (!result) return;
+    else if (!(result instanceof Error)) break;
+    else {
+      if (n == 5) {
+        throw result;
+      }
+      Utilities.sleep((Math.pow(2, n) * 1000) + (Math.round(Math.random() * 1000)));
+    }
+  }
+  if (method == "post" && JSON.parse(result)['name']) return JSON.parse(result)['name'];
+  // Sometimes JSON.parse() fails with "Unexpected token: <"
+  try {
+    return JSON.parse(result);
+  }
+  catch (e) {
+    throw new Error(result);
+  }
 };
 
 FirebaseApp_._sendRequest = function (url, params) {
-	// Added Try-catch as fetch method can fail even with muteHttpExceptions
-	// Usually when it times out - ie: when Firebase is tacking too long to answer (more than 60s)
-	try {
-		var result = UrlFetchApp.fetch(url, params);
-	}
-	catch (e) {
-		// in case of timeout, if we are writing data, assume firebase will eventually write
-		// ie don't throw error and continue to work
-		if (params.method  == "post" || params.method  == "put" || params.method == "delete") return;
-		else return new Error("We're sorry, a server error occurred. Please wait a bit and try again.");
-	}
-	var responseCode = result.getResponseCode();
-	// print=silent returns a 204 No Content on success
-	if (responseCode == 204) return;
-	// Avoid returning the Firebase app secret in case of error
-	if (result.toString().indexOf('auth=') != -1) {
-		return new Error("We're sorry, a server error occurred. Please wait a bit and try again.");
-	}
-	if (responseCode == 400 || responseCode == 401 || responseCode == 500 || responseCode == 502) {
-		if (result.toString().indexOf('error') != -1) return new Error(JSON.parse(result).error);
-		else return new Error("We're sorry, a server error occurred. Please wait a bit and try again.");
-	}
-	return result;
+  // Added Try-catch as fetch method can fail even with muteHttpExceptions
+  // Usually when it times out - ie: when Firebase is tacking too long to answer (more than 60s)
+  try {
+    var result = UrlFetchApp.fetch(url, params);
+  }
+  catch (e) {
+    // in case of timeout, if we are writing data, assume firebase will eventually write
+    // ie don't throw error and continue to work
+    if (params.method == "post" || params.method == "put" || params.method == "delete") return;
+    else return new Error("We're sorry, a server error occurred. Please wait a bit and try again.");
+  }
+  var responseCode = result.getResponseCode();
+  // print=silent returns a 204 No Content on success
+  if (responseCode == 204) return;
+  // Avoid returning the Firebase app secret in case of error
+  if (result.toString().indexOf('auth=') != -1) {
+    return new Error("We're sorry, a server error occurred. Please wait a bit and try again.");
+  }
+  if (responseCode == 400 || responseCode == 401 || responseCode == 500 || responseCode == 502) {
+    if (result.toString().indexOf('error') != -1) return new Error(JSON.parse(result).error);
+    else return new Error("We're sorry, a server error occurred. Please wait a bit and try again.");
+  }
+  return result;
 };
